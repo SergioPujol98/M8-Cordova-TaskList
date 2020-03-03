b3ad5884667917141745b93228debfe303288963
SergioPujol98 <sergiopujol.98@gmail.com>
2020-03-03 16:29:28 +0100

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,35 @@
+
+function afegirTasca() {
+    	var text = ("Afegir tasca: ");
+    	var nouElement = $(<li><a \ class="ui-btn ui-btn-icon-right ui-icon-ca" href="#info"></li>)
+    }
+
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
